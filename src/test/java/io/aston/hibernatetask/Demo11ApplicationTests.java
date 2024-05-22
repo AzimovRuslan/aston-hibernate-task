d17ebf43eda4e46f719d280290ec38edfa7aae1f
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2024-05-22 18:08:38 +0300

    init commit

---
+++ src/test/java/io/aston/hibernatetask/Demo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aston.hibernatetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
