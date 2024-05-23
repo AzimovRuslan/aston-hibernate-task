afda93ddcdf5ad929b938a831155a384afbd1590
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2024-05-24 02:26:41 +0300

    has been edited Model mapper config

--- src/main/java/io/aston/hibernatetask/config/ModelMapperConfig.java
+++ src/main/java/io/aston/hibernatetask/config/ModelMapperConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-
-
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
